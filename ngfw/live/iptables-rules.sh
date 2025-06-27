0665681648f1f4372272d51bcd8a67a70ac1e397
PaulBerra <97039294+PaulBerra@users.noreply.github.com>
2025-06-27 22:06:40 +0200

    Delete ngfw directory

--- ngfw/live/iptables-rules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# iptables rules - TinyCTI Export
-# Generated: 2025-06-27T21:37:07.235656
-
-# Drop malicious IPs
-iptables -A INPUT -s 162.243.103.246 -j DROP
-iptables -A FORWARD -s 162.243.103.246 -j DROP
